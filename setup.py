1acc056358b9cfe4809253fad226d52fdf0501d3
DaniGarciaPerez <83450345+DaniGarciaPerez@users.noreply.github.com>
2023-09-26 23:17:37 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo to explore the implementation of Transformers from scratch and its application to Time Series.',
+    author='Dani Garcia',
+    license='MIT',
+)

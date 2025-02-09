1fd683b7676d4e5635611a1a77d4716c7bc44b9d
Daniel Garcia <danielgarciache@gmail.com>
2025-02-09 09:58:08 +0100

    Add fix to positional encoding: add 2i factor

--- src/blocks/positional_encoding.py
+++ src/blocks/positional_encoding.py
@@ -14,7 +14,7 @@ class PositionalEncoding(nn.Module):
         super(PositionalEncoding, self).__init__()
         self.input_matrix = input_matrix
         self.sequence_length = input_matrix.shape[0]
-        self.vector_dim = input_matrix.shape[1]
+        self.d_model = input_matrix.shape[1]
 
     def generate_positional_encoding_values(self) -> torch.tensor:
         """
@@ -27,13 +27,15 @@ class PositionalEncoding(nn.Module):
 
         """
         # Create a tensor of indices from 0 to 'vector_dim - 1' with float data type to capture embeddings length.
-        embedding_indices = torch.arange(self.vector_dim, dtype=torch.float)
+        embedding_indices = torch.arange(self.d_model, dtype=torch.float)
         # Calculate an exponential factor for positional encoding using a base of 10000.
-        exponential_block = torch.pow(10000, (embedding_indices / self.vector_dim))
+        exponential_block = torch.pow(
+            10000, (2 * (embedding_indices // 2)) / self.d_model
+        )  # TODO: Review the exponential block
         # Create a matrix of token indices along the sequence length.
         # Each row corresponds to a position in the sequence, and columns represent different embedding dimensions.
         tokens_index_matrix = (
-            torch.arange(self.sequence_length).unsqueeze(1).expand(-1, self.vector_dim)
+            torch.arange(self.sequence_length).unsqueeze(1).expand(-1, self.d_model)
         )
         # Calculate the positional encoding by dividing token indices by the exponential factor.
         positional_encoding = tokens_index_matrix / exponential_block

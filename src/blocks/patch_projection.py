cc0f0e7318e78b7e612bdc8a86878baf0f40267a
DaniGarciaPerez <danielgarciache@gmail.com>
2025-02-22 16:46:09 +0100

    Add advances on the linear patch projection module

--- src/blocks/patch_projection.py
+++ src/blocks/patch_projection.py
@@ -9,13 +9,12 @@ linear layer to get crops for the image to process.
 import cv2 as cv2
 import torch
 from torch import nn
-from torchvision import transforms
-import numpy as np
 
 
 class PatchLinearProjection(nn.Module):
+    """TODO: Add docstring"""
 
-    def __init__(self):
+    def __init__(self, d_model):
         """
         Initializes the LinearPatchProjection module.
 
@@ -23,6 +22,8 @@ class PatchLinearProjection(nn.Module):
         """
         # Instantiate the base class
         super(PatchLinearProjection, self).__init__()
+        self.d_model = d_model
+        self.linear_weights = nn.Linear(d_model, d_model)
 
     def split_image_patches(self, image: str):
         """TODO: Add docstring"""
@@ -32,8 +33,15 @@ class PatchLinearProjection(nn.Module):
             torch.from_numpy(cv2.imread(image)).to(torch.float32).movedim(2, 0)
         )
 
-        size = 18
-        unfold = nn.Unfold(kernel_size=(size, size), stride=size)
+        unfold = nn.Unfold(
+            kernel_size=(self.d_model, self.d_model), stride=self.d_model
+        )
         unfolded_image = torch.transpose(unfold(image_matrix), 0, 1)
 
         return torch.nn.functional.normalize(unfolded_image)
+
+    def forward(self, image_to_transform) -> torch.tensor:
+        """TODO: Add docstring"""
+
+        image_patches = self.split_image_patches(image_to_transform)
+        return self.linear_weights(image_patches)

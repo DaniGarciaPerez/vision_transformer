b3e015d561a5a0702e46d47b7487cbbda65230af
DaniGarciaPerez <danielgarciache@gmail.com>
2025-03-08 16:00:56 +0100

    Add env variables and optimizer class

--- src/main.py
+++ src/main.py
@@ -4,40 +4,57 @@ Author: Dani Garcia
 Description: TODO: Insert
 
 """
-
+import os
+from pathlib import Path
 import torch
+from dotenv import load_dotenv
 from src.model_components.vit import VisionTransformer
 from src.training_utils.dataset_loader import LoadDataset
 from src.training_utils.train import TrainModel
+from src.training_utils.optimizer import LoadOptimizer
 
-D_MODEL = 256
-N_CLASSES = 100
-PATCH_SIZE = 16
-N_HEADS = 12
-MLP_SIZE = 3072
-N_LAYERS = 12
-DROPOUT_RATIO = 0.1
-EPOCHS = 3
-N_CHANNELS = 3
-BATCH_SIZE = 200
-LEARNING_RATE = 0.001
 
-model = VisionTransformer(
-    PATCH_SIZE,
-    D_MODEL,
-    MLP_SIZE,
-    N_HEADS,
-    DROPOUT_RATIO,
-    N_LAYERS,
-    N_CLASSES,
-    N_CHANNELS,
-    BATCH_SIZE,
-)
 
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-training_loader, validation_loader = LoadDataset().load_data(BATCH_SIZE, "CIFAR100")
+if __name__=="__main__":
+    load_dotenv(os.path.join(Path(__file__).parent.parent, "parameters.env"))
 
-model_trainer = TrainModel(model, loss_fn, optimizer, EPOCHS)
-model_trainer.train(training_loader, validation_loader)
+    D_MODEL = int(os.getenv("D_MODEL"))
+    N_CLASSES = int(os.getenv("N_CLASSES"))
+    PATCH_SIZE = int(os.getenv("PATCH_SIZE"))
+    N_HEADS = int(os.getenv("N_HEADS"))
+    MLP_SIZE = int(os.getenv("MLP_SIZE"))
+    N_LAYERS = int(os.getenv("N_LAYERS"))
+    DROPOUT_RATIO = float(os.getenv("DROPOUT_RATIO"))
+    EPOCHS = int(os.getenv("EPOCHS"))
+    N_CHANNELS = int(os.getenv("N_CHANNELS"))
+    BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
+    LEARNING_RATE = float(os.getenv("LEARNING_RATE"))
+    DATASET = os.getenv("DATASET", "FashionMnist")
+    OPTIMIZER = os.getenv("OPTIMIZER", "ADAM")
+
+    # Load model and insert training parameters
+    model = VisionTransformer(
+        PATCH_SIZE,
+        D_MODEL,
+        MLP_SIZE,
+        N_HEADS,
+        DROPOUT_RATIO,
+        N_LAYERS,
+        N_CLASSES,
+        N_CHANNELS,
+        BATCH_SIZE,
+    )
+
+    # Define loss for multiclass classification
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    # Define optimizer
+    optimizer = LoadOptimizer(model.parameters(), OPTIMIZER, LEARNING_RATE).get_optimizer()
+
+    # Load training and validation datasets
+    training_loader, validation_loader = LoadDataset(dataset_name=DATASET).load_data(BATCH_SIZE)
+
+    # Train the model
+    model_trainer = TrainModel(model, loss_fn, optimizer, EPOCHS)
+    model_trainer.train(training_loader, validation_loader)

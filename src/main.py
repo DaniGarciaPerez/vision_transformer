a32b6475298199f0223145695d2d1906df256f55
DaniGarciaPerez <danielgarciache@gmail.com>
2025-03-21 18:20:08 +0100

    Refactoring. Refactor main script

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 """
 Author: Dani Garcia
 
-Description: TODO: Insert
+Description: 
+    Main entry point for training a Vision Transformer (ViT) model on a specified dataset.
 
+Usage:
+    python main.py
 """
+
 import os
 from pathlib import Path
 import torch
@@ -13,12 +17,12 @@ from src.training_utils.dataset_loader import LoadDataset
 from src.training_utils.train import TrainModel
 from src.training_utils.optimizer import LoadOptimizer
 
+if __name__ == "__main__":
 
-
-
-if __name__=="__main__":
+    # Load environment variables from.env file
     load_dotenv(os.path.join(Path(__file__).parent.parent, "parameters.env"))
 
+    # Model and training HP
     D_MODEL = int(os.getenv("D_MODEL"))
     N_CLASSES = int(os.getenv("N_CLASSES"))
     PATCH_SIZE = int(os.getenv("PATCH_SIZE"))
@@ -33,28 +37,32 @@ if __name__=="__main__":
     DATASET = os.getenv("DATASET", "FashionMnist")
     OPTIMIZER = os.getenv("OPTIMIZER", "ADAM")
 
-    # Load model and insert training parameters
+    # Instantiate ViT
     model = VisionTransformer(
-        PATCH_SIZE,
-        D_MODEL,
-        MLP_SIZE,
-        N_HEADS,
-        DROPOUT_RATIO,
-        N_LAYERS,
-        N_CLASSES,
-        N_CHANNELS,
-        BATCH_SIZE,
+        patch_size=PATCH_SIZE,
+        d_model=D_MODEL,
+        mlp_size=MLP_SIZE,
+        n_heads=N_HEADS,
+        dropout_ratio=DROPOUT_RATIO,
+        n_layers=N_LAYERS,
+        n_classes=N_CLASSES,
+        n_channels=N_CHANNELS,
+        batch_size=BATCH_SIZE,
     )
 
-    # Define loss for multiclass classification
+    # Define loss function
     loss_fn = torch.nn.CrossEntropyLoss()
 
-    # Define optimizer
-    optimizer = LoadOptimizer(model.parameters(), OPTIMIZER, LEARNING_RATE).get_optimizer()
+    # Instantiate Optimizer
+    optimizer = LoadOptimizer(
+        model_parameters=model.parameters(),
+        optimizer=OPTIMIZER,
+        learning_rate=LEARNING_RATE
+    ).get_optimizer()
 
-    # Load training and validation datasets
-    training_loader, validation_loader = LoadDataset(dataset_name=DATASET).load_data(BATCH_SIZE)
+    # Load Training and Validation Datasets
+    training_loader, validation_loader = LoadDataset(dataset_name=DATASET).load_data(batch_size=BATCH_SIZE)
 
-    # Train the model
-    model_trainer = TrainModel(model, loss_fn, optimizer, EPOCHS)
+    # Train the Model
+    model_trainer = TrainModel(model, loss_fn, optimizer, epochs=EPOCHS)
     model_trainer.train(training_loader, validation_loader)

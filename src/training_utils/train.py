2a7be204007f39e024f0f5481307f1f1ec2e0e10
DaniGarciaPerez <danielgarciache@gmail.com>
2025-04-07 09:16:32 +0200

    COMMENTS. Add reference to training code in Pytorch repo

--- src/training_utils/train.py
+++ src/training_utils/train.py
@@ -3,6 +3,9 @@ Author: Dani Garcia
 
 Description: TODO: Insert
 
+
+Code reference for some of this code: https://pytorch.org/tutorials/beginner/nn_tutorial.html#training-a-model
+
 """
 
 import torch
@@ -12,6 +15,9 @@ from datetime import datetime
 
 
 class TrainModel:
+    """
+    TODO: Insert docstring
+    """
     def __init__(self, model: object, loss_fn: object, optimizer: object, epochs: int):
         self.model = model
         self.loss_fn = loss_fn
@@ -22,30 +28,23 @@ class TrainModel:
         """
         TODO: Insert docstring
         """
+
         running_loss = 0.0
         last_loss = 0.0
 
-        # Here, we use enumerate(training_loader) instead of
-        # iter(training_loader) so that we can track the batch
-        # index and do some intra-epoch reporting
         for i, data in tqdm(enumerate(training_loader)):
-            # Every data instance is an input + label pair
+
             inputs, labels = data
 
-            # Zero your gradients for every batch!
             self.optimizer.zero_grad()
 
-            # Make predictions for this batch
             outputs = self.model(inputs)
 
-            # Compute the loss and its gradients
             loss = self.loss_fn(outputs, labels)
             loss.backward()
 
-            # Adjust learning weights
             self.optimizer.step()
 
-            # Gather data and report
             running_loss += loss.item()
             if i % 50 == 49:
                 last_loss = running_loss / 50  # loss per batch
@@ -61,7 +60,6 @@ class TrainModel:
         TODO: Insert docstring
         """
 
-        # Initializing in a separate cell so we can easily add more epochs to the same run
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         writer = SummaryWriter("runs/fashion_trainer_{}".format(timestamp))
         epoch_number = 0
@@ -72,16 +70,13 @@ class TrainModel:
 
             print("EPOCH {}:".format(epoch_number + 1))
 
-            # Make sure gradient tracking is on, and do a pass over the data
             self.model.train(True)
             avg_loss = self.one_epoch(epoch_number, writer, training_loader)
 
             running_vloss = 0.0
-            # Set the model to evaluation mode, disabling dropout and using population
-            # statistics for batch normalization.
+
             self.model.eval()
 
-            # Disable gradient computation and reduce memory consumption.
             with torch.no_grad():
                 for i, vdata in enumerate(validation_loader):
                     vinputs, vlabels = vdata
@@ -92,8 +87,6 @@ class TrainModel:
             avg_vloss = running_vloss / (i + 1)
             print("LOSS train {} valid {}".format(avg_loss, avg_vloss))
 
-            # Log the running loss averaged per batch
-            # for both training and validation
             writer.add_scalars(
                 "Training vs. Validation Loss",
                 {"Training": avg_loss, "Validation": avg_vloss},
@@ -101,7 +94,6 @@ class TrainModel:
             )
             writer.flush()
 
-            # Track best performance, and save the model's state
             if avg_vloss < best_vloss:
                 best_vloss = avg_vloss
                 model_path = "model_{}_{}".format(timestamp, epoch_number)
